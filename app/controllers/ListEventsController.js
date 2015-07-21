4a4e99455348a8fb3c2f3b80acd7f65efc6cd538
Mahtab <mahtabz@gmail.com>
2015-07-21 18:52:58 -0400

    Added functionality for promoted events

--- app/controllers/ListEventsController.js
+++ app/controllers/ListEventsController.js
@@ -1,6 +1,7 @@
 GameUpApp.controller('ListEventsController', ['$scope', '$location', function($scope, $location){
 
   $scope.eventList = [];
+  $scope.promotedEventList = [];
 
   $scope.retrieveEventList = function(){
     var events = Parse.Object.extend("Event");
@@ -9,9 +10,20 @@ GameUpApp.controller('ListEventsController', ['$scope', '$location', function($s
     return query.find();
   };
 
+  $scope.retrievePromotedEventList = function(){
+    var query = new Parse.Query("Event");
+    query.equalTo("isPromoted", true);
+    return query.find();
+  };
+
   $scope.retrieveEventList().then(function(data) {
     $scope.eventList.push(data);
   });
+
+  $scope.retrievePromotedEventList().then(function(data) {
+    $scope.promotedEventList.push(data);
+  });
+
 
   $scope.formatDate = function(date){
     var date = date.split("-").join("/");
