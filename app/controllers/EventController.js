3fdf092116306b0efe8016b840ea2501c14c6f34
crestwood-test <tyler.haugen.stanley@crestwood.on.ca>
2015-07-22 11:24:23 -0400

    Removed debugger code

--- app/controllers/EventController.js
+++ app/controllers/EventController.js
@@ -5,19 +5,10 @@ GameUpApp.controller('EventController', ['$scope', '$location', 'events', functi
     var gameName = form.eventgame.gameObject.attributes.Name;
 
     angular.forEach($scope.allGameObjects, function(value){
-      debugger;
       if (value.gameObject.attributes.Name == gameName){
         gameObject = value.gameObject;
       }
     });
-    /*
-    for (int i = 0; i < $scope.allGameObjects; i ++){
-      var currentObject = allGameObjects[i].gameObject
-      if (currentObject.attributes.Name == gameName){
-        gameObject = currentObject;
-      }
-    }
-    */
 
     events.create(form, gameObject)
       .then(function(){
@@ -26,18 +17,6 @@ GameUpApp.controller('EventController', ['$scope', '$location', 'events', functi
         $scope.error = "There was an error saving your event.";
       })
   };
-
-  // $scope.allGames = 
-  // [
-  //   {
-  //     gameTitle: "Test"
-  //   },
-  //   {
-  //     gameTitle: "Test2"
-  //   }
-  // ]
-
-  // $scope.form = {type : $scope.allGames[0].gameTitle};
 
   $scope.populateGamesList = function(){
 
