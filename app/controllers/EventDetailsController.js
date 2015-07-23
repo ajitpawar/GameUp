161f8794aecfd5fc85656288644b022b0a00909f
Ajit Pawar <me@pawar.ca>
2015-07-23 11:51:15 -0400

    charge user via stripe

--- app/controllers/EventDetailsController.js
+++ app/controllers/EventDetailsController.js
@@ -7,4 +7,27 @@ GameUpApp.controller('EventDetailsController', ['$scope', '$location', 'eventSer
     return dateOut;
   };
 
+  $scope.chargeStripeCardOfUser = function(){
+      var customerID = "cus_6eu21pogeFxGh6";
+      var amount = 650;
+
+      Parse.Cloud.run('chargeStripeCardOfUser',
+        { customerID: customerID,
+          amount: amount
+        },
+        { success: function(resp) {
+          console.log(resp);
+          var mssg = "Your "+resp.source.brand+" was successfully charged $"+ (resp.amount)/100 + " " + resp.currency;
+          // $scope.alerts.push({type:"", mssg:mssg});
+          console.log(mssg);
+
+          // save in Parse.Tickets //
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
+    };
+
+
 }]);
